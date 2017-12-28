002f862a7efc886aa53894e8fc75eb011bcf1240
xinghang.zhou <13633843273@163.com>
2017-12-28 20:14:25 +0800

    activeMq测试

--- taotao-manager/taotao-manager-service/src/test/java/com/taotao/activemq/TestActiveMq.java
+++ taotao-manager/taotao-manager-service/src/test/java/com/taotao/activemq/TestActiveMq.java
@@ -23,7 +23,7 @@ public class TestActiveMq {
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         //5.使用Session对象创建一个Destination对象，两种形式queue、topic。现在应该使用queue
         //参数就是消息队列的名称
-        Queue queue = session.createQueue("test-queue2");
+        Queue queue = session.createQueue("test-queue");
         //6.使用Session对象创建一个Producer对象
         MessageProducer producer = session.createProducer(queue);
         //7.创建一个TextMessage对象
@@ -37,5 +37,50 @@ public class TestActiveMq {
         session.close();
         connection.close();
     }
+
+    @Test
+    public void testQueueConsumer() throws Exception {
+        //创建一个连接工厂对象
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        //使用连接工厂对象创建一个连接
+        Connection connection = connectionFactory.createConnection();
+        //开启连接
+        connection.start();
+        //使用连接对象创建一个Session对象
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //使用Session创建一个Destination，Destination应该和消息的发送端一致。
+        Queue queue = session.createQueue("test-queue");
+        //使用Session创建一个Consumer对象
+        MessageConsumer consumer = session.createConsumer(queue);
+        //向Consumer对象中设置一个MessageListener对象，用来接收消息
+        consumer.setMessageListener(new MessageListener() {
+
+            @Override
+            public void onMessage(Message message) {
+                //取消息的内容
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    try {
+                        String text = textMessage.getText();
+                        //打印消息内容
+                        System.out.println(text);
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        //系统等待接收消息
+		/*while(true) {
+			Thread.sleep(100);
+		}*/
+        System.in.read();
+        //关闭资源
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+
 
 }
