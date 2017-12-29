c76d07b2ed0d9ea1cc181fae32cb1eb1181df749
xinghang.zhou <13633843273@163.com>
2017-12-29 15:54:06 +0800

    FreeMarker常用标签

--- taotao-item-web/src/test/java/com/taotao/freemarker/FreeMarkerTest.java
+++ taotao-item-web/src/test/java/com/taotao/freemarker/FreeMarkerTest.java
@@ -9,8 +9,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by admin on 2017/12/29.
@@ -27,10 +26,24 @@ public class FreeMarkerTest {
         //4设置字符集
         configuration.setDefaultEncoding("utf-8");
         //5使用Configuration对象加载一个模板文件
-        Template template = configuration.getTemplate("hello.ftl");
+        Template template = configuration.getTemplate("student.ftl");
         //6创建一个数据集，可以是pojo，也可以是map，推荐map
         Map data = new HashMap<>();
         data.put("hello", "hello FreeMarker");
+        Student student = new Student(1, "小米", 11, "北京昌平回龙观");
+        data.put("student", student);
+        List<Student> stuList = new ArrayList<>();
+        stuList.add(new Student(1, "小米", 11, "北京昌平回龙观"));
+        stuList.add(new Student(2, "小米2", 12, "北京昌平回龙观"));
+        stuList.add(new Student(3, "小米3", 13, "北京昌平回龙观"));
+        stuList.add(new Student(4, "小米4", 14, "北京昌平回龙观"));
+        stuList.add(new Student(5, "小米5", 15, "北京昌平回龙观"));
+        stuList.add(new Student(6, "小米6", 16, "北京昌平回龙观"));
+        stuList.add(new Student(7, "小米7", 17, "北京昌平回龙观"));
+        data.put("stuList", stuList);
+        //日期类型的处理
+        data.put("date", new Date());
+        data.put("val","123456");
         //7创建Writer对象，制定输出文件
         Writer out= new FileWriter(new File("F:/freemarker.html"));
         //8使用模板对象的process方法输出文件
