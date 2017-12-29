a14085086a140d6be7af161e2d19865d8c7a05cb
xinghang.zhou <13633843273@163.com>
2017-12-29 14:55:40 +0800

    manager-service添加redis缓存功能

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -6,15 +6,20 @@ import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.util.IDUtils;
+import com.taotao.common.util.JsonUtils;
 import com.taotao.dao.TbItemDao;
 import com.taotao.dao.TbItemDescDao;
+import com.taotao.jedis.JedisClient;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemQuery;
 import com.taotao.service.ItemService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -29,6 +34,7 @@ import java.util.List;
 
 @Service
 public class ItemServiceImpl implements ItemService {
+    private static final Logger logger = LoggerFactory.getLogger(ItemServiceImpl.class);
     @Autowired
     private TbItemDao tbItemDao;
     @Autowired
@@ -37,14 +43,57 @@ public class ItemServiceImpl implements ItemService {
     private JmsTemplate jmsTemplate;
     @Resource(name = "itemAddtopic")
     private Destination destination;
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${ITEM_INFO}")
+    private String ITEM_INFO;
+    @Value("${ITEM_EXPIRE}")
+    private Integer ITEM_EXPIRE;
 
     @Override
     public TbItem getItemById(long itemId) {
-        return tbItemDao.selectByPrimaryKey(itemId);
+        //查询数据前先查询缓存
+        try {
+            String json = jedisClient.get(ITEM_INFO + ":" + itemId + ":BASE");
+            if(StringUtils.isNotBlank(json)) {
+                return JsonUtils.jsonToPojo(json, TbItem.class);
+            }
+        } catch (Exception e) {
+            logger.error("ItemServiceImpl.getItemById.ERROR : {}", e);
+        }
+        //缓存未命中，查询数据库
+        TbItem tbItem = tbItemDao.selectByPrimaryKey(itemId);
+        try {
+            //结果添加到缓存
+            jedisClient.set(ITEM_INFO + ":" + itemId + ":BASE", JsonUtils.objectToJson(tbItem));
+            //设置过期时间，提高缓存利用率
+            jedisClient.expire(ITEM_INFO + ":" + itemId + ":BASE", ITEM_EXPIRE);
+        } catch (Exception e) {
+            logger.error("ItemServiceImpl.getItemById.ERROR : {}", e);
+        }
+        return tbItem;
     }
 
     @Override
     public TbItemDesc getItemDescById(long itemId) {
+        //查询数据前，先查询缓存
+        try {
+            String json = jedisClient.get(ITEM_INFO + ":" + itemId + ":DESC");
+            if(StringUtils.isNotBlank(json)) {
+                return JsonUtils.jsonToPojo(json, TbItemDesc.class);
+            }
+        } catch (Exception e){
+            logger.error("ItemServiceImpl.getItemDescById.ERROR : {}", e);
+        }
+        //缓存未命中，查询数据库
+        TbItemDesc tbItemDesc = tbItemDescDao.selectByPrimaryKey(itemId);
+        try {
+            jedisClient.set(ITEM_INFO + ":" + itemId + ":DESC", JsonUtils.objectToJson(tbItemDesc));
+            jedisClient.expire(ITEM_INFO + ":" + itemId + ":DESC", ITEM_EXPIRE);
+        } catch (Exception e) {
+            logger.error("ItemServiceImpl.getItemDescById.ERROR : {}", e);
+        }
         return tbItemDescDao.selectByPrimaryKey(itemId);
     }
 
