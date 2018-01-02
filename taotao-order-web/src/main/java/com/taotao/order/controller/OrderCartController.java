0ed193c2fa1ddca91fd11655962b3f7bf7d46965
xinghang.zhou <13633843273@163.com>
2018-01-02 21:02:07 +0800

    订单生成接口完成

--- taotao-order-web/src/main/java/com/taotao/order/controller/OrderCartController.java
+++ taotao-order-web/src/main/java/com/taotao/order/controller/OrderCartController.java
@@ -1,13 +1,20 @@
 package com.taotao.order.controller;
 
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.util.CookieUtils;
 import com.taotao.common.util.JsonUtils;
+import com.taotao.order.pojo.OrderInfo;
+import com.taotao.order.service.OrderService;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbUser;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -20,6 +27,9 @@ import java.util.List;
 
 @Controller
 public class OrderCartController {
+    @Autowired
+    private OrderService orderService;
+
     @Value("${CART_KEY}")
     private String CART_KEY;
 
@@ -32,6 +42,27 @@ public class OrderCartController {
         request.setAttribute("cartList", items);
         return "/order-cart";
     }
+
+    /**
+     * 生成订单处理
+     * @param orderInfo
+     * @param model
+     * @return
+     */
+    @PostMapping("/order/create")
+    public String createOrder(OrderInfo orderInfo, Model model) {
+        //生成订单
+        TaotaoResult taotaoResult = orderService.createOrder(orderInfo);
+        //返回逻辑视图
+        model.addAttribute("orderId", taotaoResult.getData().toString());
+        model.addAttribute("payment", orderInfo.getPayment());
+        //TODO 预计送达时间，暂定为3天后送达
+        DateTime dateTime = new DateTime();
+        dateTime = dateTime.plusDays(3);
+        model.addAttribute("date", dateTime.toString("yyyy-MM-dd"));
+        return "success";
+    }
+
 
 
     private List<TbItem> getCartItemList(HttpServletRequest request) {
